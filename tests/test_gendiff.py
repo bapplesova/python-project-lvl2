98e898122cd0b1b08b881a1be7ce7af473646f26
Veronika B <bapplesova@gmail.com>
2022-01-06 18:32:53 +0300

    edit tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -21,8 +21,3 @@ def test_generate_diff_json_yaml():
 def test_generate_diff_yaml_json():
     diff_result = str(generate_diff('tests/fixtures/file1.yaml', 'tests/fixtures/file2.json', 'stylish'))
     assert diff_result == right_answer
-
-
-def test_generate_diff_nested_json():
-    diff_result = str(generate_diff('tests/fixtures/f1.json', 'tests/fixtures/f2.json', 'stylish'))
-    assert diff_result == right_nested_answer
