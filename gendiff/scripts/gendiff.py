836fa74e692883d5e20a9802c9106c1288177908
Veronika B <bapplesova@gmail.com>
2022-01-19 17:22:09 +0300

    edit format unpacking

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -26,9 +26,7 @@ def generate_diff(first_file, second_file, *format):
     # сравниваем 2 файла, формируем словарь с отличиями
     dictionary_difference = generate_difference(new_file, old_file)
     # выводим отличия по заданному виду
-    print('FORMAT', format, type(format))
-    required_format = str(format).lower()
-    print('FORMAT', format, type(format), required_format)
+    required_format = str(format[0]).lower()
     if required_format == 'plain':
         result = collect_plain_result(dictionary_difference, '')
     elif required_format == 'json':
