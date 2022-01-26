312d5c3196e49eeb1e75600804c4dd271b0cd508
Veronika B <bapplesova@gmail.com>
2022-01-26 11:20:51 +0300

    simplify test_gendiff

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -16,52 +16,50 @@ format_plain = 'plain'
 format_json = 'json'
 
 file_stylish_simple = 'tests/fixtures/answer_stylish_simple'
-answer_stylish_simple = open(file_stylish_simple, "r").read()
 file_stylish_nested = 'tests/fixtures/answer_stylish_nested'
-answer_stylish_nested = open(file_stylish_nested, "r").read()
 
 file_plain_simple = 'tests/fixtures/answer_plain_simple'
-answer_plain_simple = open(file_plain_simple, "r").read()
 file_plain_nested = 'tests/fixtures/answer_plain_nested'
-answer_plain_nested = open(file_plain_nested, "r").read()
 
 file_json_simple = 'tests/fixtures/answer_json_simple.json'
-answer_json_simple = open(file_json_simple, "r").read()
 file_json_nested = 'tests/fixtures/answer_json_nested.json'
-answer_json_nested = open(file_json_nested, "r").read()
 
 
 @pytest.mark.parametrize(
-    "file1, file2, output_format, expected",
-    [(file1_simple_json, file2_simple_json, format_stylish, answer_stylish_simple),
-     (file1_simple_json, file2_simple_yaml, format_stylish, answer_stylish_simple),
-     (file1_simple_yaml, file2_simple_json, format_stylish, answer_stylish_simple),
-     (file1_simple_yaml, file2_simple_yaml, format_stylish, answer_stylish_simple),
+    "file1, file2, output_format, answer_file",
+    [(file1_simple_json, file2_simple_json, format_stylish, file_stylish_simple),
+     (file1_simple_json, file2_simple_yaml, format_stylish, file_stylish_simple),
+     (file1_simple_yaml, file2_simple_json, format_stylish, file_stylish_simple),
+     (file1_simple_yaml, file2_simple_yaml, format_stylish, file_stylish_simple),
 
-     (file1_simple_json, file2_simple_json, format_plain, answer_plain_simple),
-     (file1_simple_json, file2_simple_yaml, format_plain, answer_plain_simple),
-     (file1_simple_yaml, file2_simple_json, format_plain, answer_plain_simple),
-     (file1_simple_yaml, file2_simple_yaml, format_plain, answer_plain_simple),
+     (file1_simple_json, file2_simple_json, format_plain, file_plain_simple),
+     (file1_simple_json, file2_simple_yaml, format_plain, file_plain_simple),
+     (file1_simple_yaml, file2_simple_json, format_plain, file_plain_simple),
+     (file1_simple_yaml, file2_simple_yaml, format_plain, file_plain_simple),
 
-     (file1_simple_json, file2_simple_json, format_json, answer_json_simple),
-     (file1_simple_json, file2_simple_yaml, format_json, answer_json_simple),
-     (file1_simple_yaml, file2_simple_json, format_json, answer_json_simple),
-     (file1_simple_yaml, file2_simple_yaml, format_json, answer_json_simple),
+     (file1_simple_json, file2_simple_json, format_json, file_json_simple),
+     (file1_simple_json, file2_simple_yaml, format_json, file_json_simple),
+     (file1_simple_yaml, file2_simple_json, format_json, file_json_simple),
+     (file1_simple_yaml, file2_simple_yaml, format_json, file_json_simple),
 
-     (file1_nested_json, file2_nested_json, format_stylish, answer_stylish_nested),
-     (file1_nested_json, file2_nested_yaml, format_stylish, answer_stylish_nested),
-     (file1_nested_yaml, file2_nested_json, format_stylish, answer_stylish_nested),
-     (file1_nested_yaml, file2_nested_yaml, format_stylish, answer_stylish_nested),
+     (file1_nested_json, file2_nested_json, format_stylish, file_stylish_nested),
+     (file1_nested_json, file2_nested_yaml, format_stylish, file_stylish_nested),
+     (file1_nested_yaml, file2_nested_json, format_stylish, file_stylish_nested),
+     (file1_nested_yaml, file2_nested_yaml, format_stylish, file_stylish_nested),
 
-     (file1_nested_json, file2_nested_json, format_plain, answer_plain_nested),
-     (file1_nested_json, file2_nested_yaml, format_plain, answer_plain_nested),
-     (file1_nested_yaml, file2_nested_json, format_plain, answer_plain_nested),
-     (file1_nested_yaml, file2_nested_yaml, format_plain, answer_plain_nested),
+     (file1_nested_json, file2_nested_json, format_plain, file_plain_nested),
+     (file1_nested_json, file2_nested_yaml, format_plain, file_plain_nested),
+     (file1_nested_yaml, file2_nested_json, format_plain, file_plain_nested),
+     (file1_nested_yaml, file2_nested_yaml, format_plain, file_plain_nested),
 
-     (file1_nested_json, file2_nested_json, format_json, answer_json_nested),
-     (file1_nested_json, file2_nested_yaml, format_json, answer_json_nested),
-     (file1_nested_yaml, file2_nested_json, format_json, answer_json_nested),
-     (file1_nested_yaml, file2_nested_yaml, format_json, answer_json_nested)])
-def test_generate(file1, file2, output_format, expected):
+     (file1_nested_json, file2_nested_json, format_json, file_json_nested),
+     (file1_nested_json, file2_nested_yaml, format_json, file_json_nested),
+     (file1_nested_yaml, file2_nested_json, format_json, file_json_nested),
+     (file1_nested_yaml, file2_nested_yaml, format_json, file_json_nested)])
+def test_generate(file1, file2, output_format, answer_file):
     diff_result = str(generate_diff(file1, file2, output_format))
-    assert diff_result == expected
+    assert diff_result == get_answer(answer_file)
+
+
+def get_answer(file_name):
+    return open(file_name, "r").read()
