9bc119e2372c8e755effbb481d1eb1355363a036
Veronika B <bapplesova@gmail.com>
2022-03-18 21:54:48 +0300

    Add func format_plain_internal

--- gendiff/formatters/formatter.py
+++ gendiff/formatters/formatter.py
@@ -8,7 +8,7 @@ from gendiff.formatters.json import format_json
 def format_diff(dictionary_difference, required_format):
     indent = 1
     if required_format == 'plain':
-        result = format_plain(dictionary_difference, '')
+        result = format_plain(dictionary_difference)
     elif required_format == 'json':
         result = format_json(dictionary_difference)
     else:
--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -1,7 +1,11 @@
 from gendiff.data_mapping import bool_to_str
 
 
-def format_plain(total_dict, key_parents, lines=''):
+def format_plain(total_dict):
+    return format_plain_internal(total_dict, '', '')[:-1]
+
+
+def format_plain_internal(total_dict, key_parents, lines=''):
     each_line = "Property '" + key_parents
     all_keys = tuple(sorted(total_dict))
     for key in all_keys:
@@ -17,7 +21,7 @@ def format_plain(total_dict, key_parents, lines=''):
             elif value[0] == 'changed':
                 lines += f"{each_line}{key}' was updated. From {format_value(value[1])} to {format_value(value[2])}\n"
         elif isinstance(value, dict):
-            lines += format_plain(value, key_parents + key + '.', '')
+            lines += format_plain_internal(value, key_parents + key + '.', '')
     return lines
 
 
